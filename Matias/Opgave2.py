bd457b125f3f22a69187857b4952b1e1c78ca993
MatiasShadow <matiasjohanobel@gmail.com>
2020-09-22 18:43:27 +0200

    øvelse i print input og if

---
+++ Matias/Opgave2.py
@@ -0,0 +1,8 @@
+text = "hej"
+print (text)
+navn = input ("Hvad Hedder du? ")
+
+if navn == "Matias":
+    print ("hej "+navn+" det er et grimt navn")
+else:
+    print ("hej "+navn+" det er et pænt navn")
---
+++ Matias/Opgave3.py
@@ -0,0 +1,8 @@
+text = "hej"
+print (text)
+navn = input ("Hvad Hedder du? ")
+
+if navn == "Matias":
+    print ("hej "+navn+" det er et pænt navn")
+else:
+    print ("hej "+navn+" det er et mega grimt navn")
