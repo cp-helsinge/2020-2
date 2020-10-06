22cbbcaffe49c6247086257ef84bd039049ba356
Continuar <PythonKursus@f.hoth.dk>
2020-10-06 18:11:05 +0200

    opgaver + sol

---
+++ markus/turtle_funk.py
@@ -0,0 +1,11 @@
+turtle.fillcolor("yellow")
+turtle.begin_fill()
+for n in range(1,50):
+    #turtle.forward(250)
+    #turtle.right(90)
+    turtle.pencolor("red")
+    turtle.forward(50)
+    turtle.right(75)
+turtle.right(45)
+turtle.forward(25)
+turtle.end_fill()
