9b0ce753b4a8d3c113a5c4a9b5512d61783a4f68
mega-giffel <alvin.riget.dk@gmail.com>
2020-10-20 17:52:16 +0200

    A poop

---
+++ Alvin/turtle_funkSquare.dk.py
@@ -0,0 +1,16 @@
+import turtle 
+
+def square():
+    turtle.speed(5)
+    turtle.forward(209)
+    turtle.left(90)
+    turtle.forward(209)
+    turtle.left(90)
+    turtle.forward(209)
+    turtle.left(90)
+    turtle.forward(209)
+    turtle.left(90)
+
+square()
+
+input()
