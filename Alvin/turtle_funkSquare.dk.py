683bce6105710862574d7fe81ed6d4171d585702
mega-giffel <alvin.riget.dk@gmail.com>
2020-10-20 17:59:22 +0200

    A big fart

--- Alvin/turtle_funkSquare.dk.py
+++ Alvin/turtle_funkSquare.dk.py
@@ -2,13 +2,13 @@ import turtle
 
 def square():
     turtle.speed(5)
-    turtle.forward(209)
+    turtle.forward(100)
     turtle.left(90)
-    turtle.forward(209)
+    turtle.forward(100)
     turtle.left(90)
-    turtle.forward(209)
+    turtle.forward(100)
     turtle.left(90)
-    turtle.forward(209)
+    turtle.forward(100)
     turtle.left(90)
 
 square()
