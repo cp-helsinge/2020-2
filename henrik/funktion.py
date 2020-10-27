d01dfde7260b10cdbc07bc7bdff70ee361b53ae6
unknown <cp-henrik@virtualplayground.dk>
2020-10-27 18:15:09 +0100

    sdsd

---
+++ henrik/funktion.py
@@ -0,0 +1,15 @@
+import turtle
+
+def trappe():
+    for n in range (1,7):
+        turtle.forward(10)
+        turtle.left(90)
+        turtle.forward(10)
+        turtle.right(90)
+        turtle.forward(10)
+    turtle.forward(20)
+
+trappe()
+
+
+#Vigtigt at huske indrykningen.. :-)
