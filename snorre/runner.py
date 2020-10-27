b82e67158f91b6121b43ff723460f2dbcdf1ab0a
Snubig1 <snubig1@gmail.com>
2020-10-27 18:09:21 +0100

    skilpade arpejde

---
+++ snorre/runner.py
@@ -0,0 +1,4 @@
+import turtle
+from sqare import elefantBalon
+
+elefantBalon(-144, -81, 3, 4)
--- snorre/skilpade_opgave.py
+++ snorre/skilpade_opgave.py
@@ -1,8 +1,16 @@
 import turtle
+import random
 turtle.shape("turtle")
-turtle.shapesize(10)
+turtle.shapesize(0.1)
 turtle.title("hello")
+turtle.speed(0)
 turtle.forward(300)
+turtle.circle(100)
+turtle.bgcolor("black")
+turtle.pencolor(1, 1, 1)
+o = 0
+for o in range(0,101):
+    turtle.circle(100-o)
 
 n = 5
 for n in range(1,5):
@@ -13,14 +21,14 @@ for n in range(1,5):
 
 turtle.home()
 o = 0
-for o in range(0,255):
+for o in range(0,361):
 
     turtle.right(1)
-    turtle.forward(100)
-    turtle.forward(-100)
-    turtle.pensize(o)
-    turtle.pencolor((0, 0, o/255))
+    turtle.forward(200)
+    turtle.forward(-200)
+    turtle.pensize(1)
+    turtle.pencolor((1, o/360,1 ))
     turtle.title(str(o))
 
 while 1 == 1:
-    turtle.right(0)
+    turtle.right(1)
