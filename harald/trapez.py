c1dc72e756681ca0d29e38361b3e65463a5ee5fb
haaarh <harald@eystein.dk>
2020-10-27 17:39:26 +0100

    ting

--- harald/trapez.py
+++ harald/trapez.py
@@ -5,7 +5,7 @@ def trapez(size):
     turtle.left(110)
     turtle.forward(75)
     turtle.left(70)
-    turtle.forward(size/4*3)
+    turtle.forward(size/10*9)
     turtle.left(70)
     turtle.forward(75)
     turtle.left(110)
