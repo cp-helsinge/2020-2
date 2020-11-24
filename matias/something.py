56556d71a0971b4e4f17d96c4af36dd02e3f358d
MatiasShadow <matiasjohanobel@gmail.com>
2020-11-24 17:41:02 +0100

    something.py

---
+++ matias/something.py
@@ -0,0 +1,14 @@
+class ost:
+    def __init__(self,farve,):
+        self.farve = farve
+        self.størrelse = størrelse = 2
+
+    def udskriv(self):
+        print(self.farve, self.størrelse)
+
+
+
+ost = ost('blå')
+
+print(ost.farve)
+print(ost.størrelse)½
