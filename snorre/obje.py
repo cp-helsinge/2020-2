4e3fc4d605fc0b2992e8d5aa347959c809621fc3
Snubig1 <snubig1@gmail.com>
2020-11-24 17:43:22 +0100

    lært at bruge class

---
+++ snorre/obje.py
@@ -0,0 +1,10 @@
+class Bil:
+    def __init__(self,farve):
+        self.farve = farve
+        self.bagage = ["h","dd","e"]
+    
+    def udskrif(self):
+        print(self.farve ,self. bagage)
+
+
+bil.udskrif
